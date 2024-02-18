ed81eb4a14059b24b3b7831dbcdb46bbe3d07b7d
Okem Onyeka <jesse007onyeka@gmail.com>
2024-02-18 23:57:36 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(cors({
+    credentials: true,
+}));
+
+const port = process.env.PORT ?? 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
+
+
